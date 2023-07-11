9cb2a97c4b00a871cdc55840451c090cbc2d5772
jpfacio <jpfacio@usp.br>
2023-07-11 16:06:37 -0300

    rnafold

--- halokit_igv.sh
+++ halokit_igv.sh
@@ -1,3 +1,22 @@
+if command -v RNAfold > /dev/null 2>&1; then
+    :
+else 
+    echo "The ViennaRNA package is required to use Halokit, to download it, follow these instructions: https://www.tbi.univie.ac.at/RNA/index.html#download"
+fi
+
+if command -v bedtools > /dev/null 2>&1; then
+    :
+else
+    echo "Bedtools is required to use Halokit, to download it, follow these instructions: https://bedtools.readthedocs.io/en/latest/content/installation.html"
+fi
+
+if find ~ -name "igv.sh" > /dev/null 2>&1; then
+    :
+else 
+    echo "IGV is required to use Halokit, to download it, follow these instructions: https://software.broadinstitute.org/software/igv/"
+fi
+
+
 echo '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⢠⣶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⠟⠛⢿⣶⡄⠀⢀⣀⣤⣤⣦⣤⡀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⢠⣿⠋⠀⠀⠈⠙⠻⢿⣶⣶⣶⣶⣶⣶⣶⣿⠟⠀⠀⠀⠀⠹⣿⡿⠟⠋⠉⠁⠈⢻⣷⠀⠀⠀⠀⠀
@@ -23,3 +42,11 @@ Welcome to Halokit_IGV version pre-Alpha, the Halobacterium salinarum toolkit fo
 Input a sequence: '
 
 read sequence 
+
+temp_dir=$(mktemp -d)
+
+echo ">Sequence" > "$temp_dir/sequence.fa"
+echo "$sequence" >> "$temp_dir/sequence.fa" 
+
+RNAfold -p "$temp_dir/sequence.fa"
+
